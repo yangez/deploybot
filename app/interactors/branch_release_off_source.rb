c7c3cde46c8923a6d765c017a51993ded06c6797
Eric Yang <eric@snapdocs.com>
2018-02-01 17:21:57 -0800

    Support alternative branch names
    Previously we branched the release off of 'master' and used the
    'production' branch as the destination. This patch allows for arbitrary
    'SOURCE_BRANCH' and 'DESTINATION_BRANCH' environment variables.

--- app/interactors/branch_release_off_source.rb
+++ app/interactors/branch_release_off_source.rb
@@ -1,4 +1,4 @@
-class BranchReleaseOffMaster
+class BranchReleaseOffSource
   include Interactor
   extend Memoist
   delegate :message, to: :context
@@ -10,12 +10,12 @@ class BranchReleaseOffMaster
   end
 
   def call
-    master_sha = github.repos.branch(branch: 'master').commit.sha
-    new_branch_name = "release-#{Date.current.strftime("%Y-%m-%d")}"
+    master_sha = github.repos.branch(branch: source_name).commit.sha
+    new_branch_name = "release/#{Date.current.strftime("%Y-%m-%d")}"
 
     begin
       github.git_data.references.create ref: "refs/heads/#{new_branch_name}", sha: master_sha
-      release_url = "https://github.com/SnapDocs/snapdocs/compare/production...#{new_branch_name}"
+      release_url = "https://github.com/SnapDocs/snapdocs/compare/#{destination_name}...#{new_branch_name}"
       context.message = ":rocket: Release branch created: #{release_url}"
     rescue Github::Error::UnprocessableEntity
       context.message = "Attempted to create release branch `#{new_branch_name}`, but it already exists."
@@ -28,5 +28,13 @@ class BranchReleaseOffMaster
     Github.new oauth_token: ENV['GITHUB_OAUTH_TOKEN'], org: 'snapdocs', user: 'snapdocs', repo: 'snapdocs'
   end
   memoize :github
+
+  def source_name
+    ENV['SOURCE_BRANCH'] || 'master'
+  end
+
+  def destination_name
+    ENV['DESTINATION_BRANCH'] || 'production'
+  end
 
 end
--- spec/interactors/branch_release_off_source_spec.rb
+++ spec/interactors/branch_release_off_source_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe BranchReleaseOffMaster do
+RSpec.describe BranchReleaseOffSource do
   let(:github_stub) { double(Github) }
   before do
     allow(github_stub).to receive_message_chain('repos.branch.commit.sha').and_return("test_sha")
     allow(github_stub).to receive_message_chain('git_data.references.create')
-    allow_any_instance_of(BranchReleaseOffMaster).to receive(:github).and_return(github_stub)
+    allow_any_instance_of(BranchReleaseOffSource).to receive(:github).and_return(github_stub)
   end
 
-  subject(:branch_release_off_master) { BranchReleaseOffMaster.call }
+  subject(:branch_release_off_source) { BranchReleaseOffSource.call }
 
   it { is_expected.to be_success }
 
   it "has the correct message" do
-    expect(branch_release_off_master.message).to include "Release branch created"
+    expect(branch_release_off_source.message).to include "Release branch created"
   end
 
   context "when it's the weekend" do
